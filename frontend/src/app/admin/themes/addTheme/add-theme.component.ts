bdebb2832fca3990c3306170eeb9075eac9800b5
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-05-08 15:00:26 +0200

    feat: add modal for create a theme #208

--- frontend/src/app/admin/themes/addTheme/add-theme.component.ts
+++ frontend/src/app/admin/themes/addTheme/add-theme.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Theme } from 'src/models/quiz.model';
+import { QuizService } from 'src/services/quiz.service';
 import { ThemesService } from 'src/services/theme.service';
 
 @Component({
   selector: 'app-add-theme',
   templateUrl: './add-theme.component.html',
 })
-export class AddThemeComponent {}
+export class AddThemeComponent implements OnInit {
+  @Output() loadTheme = new EventEmitter<Theme>();
+
+  public themeForm: FormGroup;
+  themes: Theme[] = [];
+  theme: Theme;
+
+  constructor(
+    private quizService: QuizService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private themeService: ThemesService
+  ) {
+    this.themeForm = this.formBuilder.group({
+      themeImage: ['', Validators.required],
+      themeName: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {}
+
+  public addTheme(): void {
+    this.theme = {
+      name: this.themeForm.value.themeName,
+      image: this.themeForm.value.themeImage,
+    };
+    this.loadTheme.emit(this.theme);
+  }
+}
