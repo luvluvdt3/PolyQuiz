99f8a695e2756bda7f619a9287b277d3f94acfc7
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-05-15 20:58:54 +0200

    feat: modification très légère du code de index.js (reprise de l'exemple du moodle), et ajouts de commentaires pour expliquer ma compréhension de ce dernier #0

--- backend/app/api/quizzes/index.js
+++ backend/app/api/quizzes/index.js
@@ -1,31 +1,38 @@
+//Imports de la classe
 const { Router } = require("express");
 
 const { Quiz } = require("../../models");
 const manageAllErrors = require("../../utils/routes/error-management");
 const QuestionsRouter = require("./questions");
 const { buildQuizz, buildQuizzes } = require("./manager");
-const ResultsRouter = require("../results");
+
+//Creation du routeur
 const router = new Router();
 
+//Affecte le questionsRouter au routeur via l'URL quizId/questions (donc gestion des questions par ce questionRouteur)
 router.use("/:quizId/questions", QuestionsRouter);
+
+//Récupération (GET) de la route / (donc de tous les quiz)
 router.get("/", (req, res) => {
   try {
-    const quizzes = buildQuizzes();
+    const quizzes = buildQuizzes(); // On appelle la méthode sans paramètre car on veut tous les récupérer
     res.status(200).json(quizzes);
   } catch (err) {
     manageAllErrors(res, err);
   }
 });
 
+// Récupération (GET) du quiz ayant pour id quiId
 router.get("/:quizId", (req, res) => {
   try {
-    const quizz = buildQuizz(req.params.quizId);
+    const quizz = buildQuizz(req.params.quizId); // Cette fois ci on passe l'id du quiz que l'on souhaite récupérer en paramètre
     res.status(200).json(quizz);
   } catch (err) {
     manageAllErrors(res, err);
   }
 });
 
+// Création (POST) d'un quiz
 router.post("/", (req, res) => {
   try {
     const quiz = Quiz.create({ ...req.body });
@@ -35,6 +42,7 @@ router.post("/", (req, res) => {
   }
 });
 
+// Mise à jour (PUT) du quiz ayant pour id quizId
 router.put("/:quizId", (req, res) => {
   try {
     res.status(200).json(Quiz.update(req.params.quizId, req.body));
@@ -43,6 +51,7 @@ router.put("/:quizId", (req, res) => {
   }
 });
 
+//Suppression (DELETE) du quiz ayant pour id quizId
 router.delete("/:quizId", (req, res) => {
   try {
     Quiz.delete(req.params.quizId);
@@ -52,4 +61,5 @@ router.delete("/:quizId", (req, res) => {
   }
 });
 
+//Exportation du routeur afin de pouvoir l'utiliser dans un autre code js
 module.exports = router;
