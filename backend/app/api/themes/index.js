73c6d36f9c0e4b126e4fe634842b44ae93d94104
Loïc PALAYER <loic.palayer@etu.univ-cotedazur.fr>
2023-05-22 09:31:54 +0200

    training: themes GET route for /

--- backend/app/api/themes/index.js
+++ backend/app/api/themes/index.js
@@ -1,16 +1,24 @@
-const { Router } = require("express");
-
-const { Theme } = require("../../models");
+const {Router} = require("express");
+const {Theme} = require("../../models");
 const manageAllErrors = require("../../utils/routes/error-management");
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-  try {
-    res.status(200).json(Theme.get());
-  } catch (err) {
-    manageAllErrors(res, err);
-  }
+// router.get("/", (req, res) => {
+//   try {
+//     res.status(200).json(Theme.get());
+//   } catch (err) {
+//     manageAllErrors(res, err);
+//   }
+// });
+
+router.get('/', async (req, res) => {
+    try {
+        const themes = await Theme.get();
+        res.status(200).json(themes);
+    } catch (err) {
+        manageAllErrors(res, err);
+    }
 });
 
 router.get("/:themeId", (req, res) => {
