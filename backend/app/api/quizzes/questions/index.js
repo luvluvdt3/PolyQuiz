f736de7c6ecc9f4f5acac9fd5d1cf90bdf23faf8
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-05-22 10:29:47 +0200

    feat: route / index.js pour la partie question #0

--- backend/app/api/quizzes/questions/index.js
+++ backend/app/api/quizzes/questions/index.js
@@ -1,22 +1,30 @@
-const { Router } = require("express");
+//Imports de la classe. Cette
 
-const { Answer, Quiz, Question } = require("../../../models");
-const manageAllErrors = require("../../../utils/routes/error-management");
 const AnswersRouter = require("./answers");
 const { filterQuestionsFromQuizz, getQuestionFromQuiz } = require("./manager");
+const { Router } = require("express");
 
+const manageAllErrors = require("../../../utils/routes/error-management");
+const { Answer, Quiz, Question } = require("../../../models");
+
+//Affecte le answersRouter au routeur via l'URL quizId/questions/questionId/answers (donc gestion des réponses par ce answersRouteur)
+router.use("/:questionId/answers", AnswersRouter);
+
+//Creation du routeur avec le paramètre mergeParams à true. Cela siginifie que les paramètres de la requête seront conservés.
 const router = new Router({ mergeParams: true });
 
+//Récupération (GET) de la route / (donc de toutes les questions)
 router.get("/", (req, res) => {
   try {
-    // Check if quizId exists, if not it will throw a NotFoundError
-    Quiz.getById(req.params.quizId);
-    res.status(200).json(filterQuestionsFromQuizz(req.params.quizId));
+    //Récupérations de tous les quiz via la variable req
+    const questions = Quiz.getById(req.params.quizId);
+    res.status(200).json(questions);
   } catch (err) {
     manageAllErrors(res, err);
   }
 });
 
+//Récupération (GET) de la question ayant pour id questionId
 router.get("/:questionId", (req, res) => {
   try {
     const question = getQuestionFromQuiz(
@@ -29,33 +37,34 @@ router.get("/:questionId", (req, res) => {
   }
 });
 
+// Création (POST) d'une question
 router.post("/", (req, res) => {
   try {
-    // Check if quizId exists, if not it will throw a NotFoundError
-    Quiz.getById(req.params.quizId);
+    //Récupérations de tous les quiz via la variable req
     const quizId = parseInt(req.params.quizId, 10);
     let question = {
       question_text: req.body.question_text,
       explain_text: req.body.explain_text,
       quizId,
     };
+    //Paramètres optionnels de la question
+    if (req.body.explain_image) {
+      question.explain_image = req.body.explain_image;
+    }
     if (req.body.question_image) {
       question.question_image = req.body.question_image;
     }
     if (req.body.question_sound) {
       question.question_sound = req.body.question_sound;
     }
-    if (req.body.explain_image) {
-      question.explain_image = req.body.explain_image;
-    }
     question = Question.create(question);
 
-    // If answers have been provided in the request, we create the answer and update the response to send.
+    // Si les réponses ont été fournis par l'utilisateur dans la requête alors on les créent également.
     if (req.body.answers && req.body.answers.length > 0) {
       const answers = req.body.answers.map((answer) =>
-        Answer.create({ ...answer, questionId: question.id })
+        Answer.create(Object.assign({}, answer, { questionId: question.id }))
       );
-      question = { ...question, answers };
+      question = Object.assign({}, question, { answers });
     }
     res.status(201).json(question);
   } catch (err) {
@@ -63,6 +72,7 @@ router.post("/", (req, res) => {
   }
 });
 
+// Mise à jour (PUT) de la question ayant pour id questionId
 router.put("/:questionId", (req, res) => {
   try {
     const question = getQuestionFromQuiz(
@@ -74,16 +84,17 @@ router.put("/:questionId", (req, res) => {
       explain_text: req.body.explain_text,
       quizId,
     };
-
+    //Paramètres optionnels de la question
+    if (req.body.explain_image) {
+      question.explain_image = req.body.explain_image;
+    }
     if (req.body.question_image) {
       question.question_image = req.body.question_image;
     }
     if (req.body.question_sound) {
       question.question_sound = req.body.question_sound;
     }
-    if (req.body.explain_image) {
-      question.explain_image = req.body.explain_image;
-    }
+
     Question.update(req.params.questionId, updatedQuestion);
     res.status(200).json(updatedQuestion);
   } catch (err) {
@@ -91,6 +102,7 @@ router.put("/:questionId", (req, res) => {
   }
 });
 
+//Suppression (DELETE) du quiz ayant pour id quizId
 router.delete("/:questionId", (req, res) => {
   try {
     // Check if the question id exists & if the question has the same quizId as the one provided in the url.
@@ -102,6 +114,5 @@ router.delete("/:questionId", (req, res) => {
   }
 });
 
-router.use("/:questionId/answers", AnswersRouter);
-
+//Exportation du routeur afin de pouvoir l'utiliser dans un autre code js
 module.exports = router;
