80dcb2dca21fc61800a339d9bbcac7abf88cb53a
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-05-27 10:52:35 +0200

    feat: travail sur le modèle d'une réponse #0

--- backend/app/models/answer.model.js
+++ backend/app/models/answer.model.js
@@ -1,9 +1,15 @@
-const Joi = require("joi");
+//Classe de modèle pour les réponses.
+
+//Afin de valider les objets JS, on va utiliser la bibliothèque de validation joi.
+const validation = require("joi");
+
+//Exportation du modèle de bases de nos modèles.
 const BaseModel = require("../utils/base-model.js");
 
+//Exportation d'un AnswerModèle, dérivant du modèle de base et prenant en compte tous les attributs définissant une réponse.
 module.exports = new BaseModel("Answer", {
-  questionId: Joi.number().required(),
-  answer_text: Joi.string(),
-  answer_image: Joi.string(),
-  isCorrect: Joi.boolean().required(),
+  questionId: validation.number().required(),
+  answer_text: validation.string(),
+  answer_image: validation.string(),
+  isCorrect: validation.boolean().required(),
 });
