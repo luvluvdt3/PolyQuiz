62e490bc0955c8c55f86a39be4a12bf368e4b831
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-05-27 10:56:48 +0200

    feat: travail de compréhension rapide sur l'index.js des modèles du backend #0

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,10 +1,13 @@
-const User = require("./user.model.js");
-const Resident = require("./resident.model.js");
+//Ce finit définit l'index de tous nos modèles. Au sein de celui ci, on va simplement importer tous les modèles de notre back,
+//puis les exporter. Regrouper tous les imports dans un même fichier nous paraît plus proche, et permets d'avoir une structure mieux organisée.
 const Quiz = require("./quiz.model.js");
-const Theme = require("./theme.model.js");
 const Question = require("./question.model.js");
 const Answer = require("./answer.model.js");
+
 const Result = require("./result.model.js");
+const User = require("./user.model.js");
+const Resident = require("./resident.model.js");
+const Theme = require("./theme.model.js");
 const Settings = require("./settings.model.js");
 const InitSettings = require("./initsettings.model.js");
 module.exports = {
