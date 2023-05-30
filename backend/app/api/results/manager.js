bc6d6a935226eda1c7f244e3c9fbbe09da668535
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-05-30 08:57:13 +0200

    feat: travail sur le manager des résultats #0

--- backend/app/api/results/manager.js
+++ backend/app/api/results/manager.js
@@ -1,20 +1,23 @@
+//Imports de la calsse
 const { Result } = require("../../models");
 const logger = require("../../utils/logger.js");
 
+// Méthode qui va permettre de créer un résultat en fonction de son id, en se servant des données de la base de données.
 const buildResult = (resultId) => {
   const result = Result.getById(resultId);
   return { result };
 };
 
 /**
- * Function buildResultzes.
- * This function aggregates the questions and answers from the database to build entire resultzes.
+ * Méthode qui, a partir des questions et des réponses de la base de données,
+ * va créer les résultats.
  */
 const buildResults = () => {
   const results = Result.get();
   return results.map((result) => buildResult(result.id));
 };
 
+//Exportation des méthodes
 module.exports = {
   buildResults,
 };
