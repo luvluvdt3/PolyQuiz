885a6f44fe2fa509857af0969f9fc9248a13d58e
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-06-03 18:32:02 +0200

    feat: travail de compréhension et de refonte sur le modèle d'un paramètre dans le back #0

--- backend/app/models/settings.model.js
+++ backend/app/models/settings.model.js
@@ -1,10 +1,18 @@
-const Joi = require("joi");
+//Classe de modèle pour les paramètres au niveau du backend. Chaque fichier se base sur le modèle de base des modèles
+//puis exporte ses attributs (ici par exemple, un paramètre est référencé par l'id de son utilisateur associé,
+//a un booléen pour l'activation du son, une string pour l'option de la souris,
+//un string pour le micro et un booléen pour la confirmation de réponse.)
+
+//Affectation de la bibliothèque de validation joi à une constante.
+const validation = require("joi");
+//Importation du modèle de base des modèles.
 const BaseModel = require("../utils/base-model.js");
 
+//Exportation d'un nouveau modèle qui va hériter du modèle de base et qui va définir les attributs d'un paramètre.
 module.exports = new BaseModel("Settings", {
-  user_id: Joi.number().required(),
-  sound_effect: Joi.boolean().required(),
-  mouse_option: Joi.string().required(),
-  microphone: Joi.string().required(),
-  confirm_answer: Joi.boolean().required(),
+  user_id: validation.number().required(),
+  sound_effect: validation.boolean().required(),
+  mouse_option: validation.string().required(),
+  microphone: validation.string().required(),
+  confirm_answer: validation.boolean().required(),
 });
